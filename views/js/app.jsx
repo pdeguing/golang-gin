3bdfc34fe7a1ab79cfdf2a118098149bf1035c35
Pierre-quentin Deguingand <pierrequentindeguingand@gmail.com>
2019-04-10 16:38:05 -0700

    Working on login

--- views/js/app.jsx
+++ views/js/app.jsx
@@ -1,4 +1,62 @@
+const AUTH0_CLIENT_ID = "nAe627MKYl0jGtkfxjKs93sp0xHbhlJm";
+const AUTH0_DOMAIN  = "dev--92ipldi.eu.auth0.com";
+const AUTH0_CALLBACK_URL = location.href;
+const AUTH0_API_AUDIENCE = "jokeish";
+
 class App extends React.Component {
+	parseHash() {
+		this.auth0 = new auth0.WebAuth({
+			domain: AUTH0_DOMAIN,
+			clientID: AUTH0_CLIENT_ID
+		});
+		this.auth0.parseHash(window.location.hash, (err, authResult) => {
+			if (err) {
+				return console.log(err);
+			}
+			if (
+				authResult !== null &&
+				authResult.accessToken !== null &&
+				authResult.idToken !== null
+			) {
+				localStorage.setItem("access_token", authResult.accessToken);
+				localStorage.setItem("id_token", authResult.idToken);
+				localStorage.setItem("profile", JSON.stringify(authResult.idTokenPayLoad));
+				window.location = window.location.href.substr(
+					0,
+					window.location.href.indexOf("#")
+				);
+			}
+		});
+	}
+
+	setup() {
+		$.ajaxSetup({
+			beforeSend: (r) => {
+				if (localStorage.getItem("access_token")) {
+					r.setRequestHeader(
+						"Authorization",
+						"Bearer " + localStorage.getItem("access_token")
+					);
+				}
+			}
+		});
+	}
+
+	setState() {
+		let idToken = localStorage.getItem("id_token");
+		if (idToken) {
+			this.loggedIn = true;
+		} else {
+			this.loggedIn = false
+		}
+	}
+
+	componentWillMount() {
+		this.setup();
+		this.parseHash();
+		this.setState();
+	}
+
 	render() {
 		if (this.loggedIn){
 			return (<LoggedIn/>);
